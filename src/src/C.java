f407644fa5d521e56f81036e386edf8266df4bf4
macdows1 <danny.mato1@gmail.com>
2015-05-15 16:11:38 -0400

    Moving to Laptop
    Just moving

--- src/src/C.java
+++ src/src/C.java
@@ -1,19 +1,33 @@
 package src;
 
+import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
 import com.jsyn.unitgen.LineOut;
-import com.jsyn.unitgen.UnitGenerator;
+import com.jsyn.unitgen.SineOscillator;
+import com.jsyn.unitgen.SquareOscillator;
+import com.jsyn.unitgen.TriangleOscillator;
+import com.jsyn.unitgen.UnitOscillator;
 
 public class C {
 	
 	private Synthesizer synth;
 	private LineOut lineOut;
-	private UnitGenerator osc;
+	private UnitOscillator osc;
 	
 	public static final double PITCH = 261.626;
 	
-	public C(UnitGenerator g){
+	public C(UnitOscillator g){
+		synth = JSyn.createSynthesizer();
+		synth.add(osc = g);
+		synth.add(lineOut = new LineOut());
 		
+		osc.frequency.set(PITCH);
+		osc.amplitude.set(1.0);
+		
+		osc.output.connect(lineOut.input);
+		
+		synth.start();
+		lineOut.start();
 	}
 
 }
